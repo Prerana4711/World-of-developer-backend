2d2589fbf00cd0037cd523c43c295c45ad925bac
Prerana@4711 <210004008@hbtu.ac.in>
2026-05-11 01:06:04 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const requestRouter = require("./routes/request")
 const userRouter = require("./routes/user")
 const cors = require("cors")
 app.use(cors({
-  origin: "https://world-of-developer-frontend-part.vercel.app/",
+  origin: "https://world-of-developer-frontend-part.vercel.app",
   credentials: true
 }))
 app.use("/",authRouter)
